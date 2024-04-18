7e69d5483807e8386759502f3b93d38c646e6219
ChrissQAng <goodomen@outlook.de>
2024-04-18 14:16:06 +0200

    id fixed

--- backend/index.js
+++ backend/index.js
@@ -24,20 +24,27 @@ app.get("/api/v1/guestbook", (_, res) => {
 // endpoint: POST
 
 app.post("/api/v1/guestbook", (req, res) => {
-  const newListItem = {
-    id: Date.now(),
-    firstName: req.body.firstName,
-    name: req.body.name,
-    email: req.body.email,
-    message: req.body.message,
-  };
   readList()
-    .then((list) => [...list, newListItem])
-    .then((newList) => writeList(newList))
-    .then((newList) => res.status(200).json(newList))
-    .catch((err) =>
-      res.status(500).json({ err, message: "Could not write new list entry" })
-    );
+    .then((list) => {
+      const idArray = list.map((item) => item.id);
+      const newId = Math.max(...idArray) + 1;
+      const newListItem = {
+        id: newId,
+        firstName: req.body.firstName,
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message,
+      };
+      const newList = [...list, newListItem];
+      writeList(newList)
+        .then((newList) => res.status(200).json(newList))
+        .catch((err) =>
+          res
+            .status(500)
+            .json({ err, message: "Could not write new list entry" })
+        );
+    })
+    .catch((err) => console.log(err));
 });
 
 const PORT = 3004;
---
+++ frontend/src/pages/homepage.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import "./Homepage.css";
+
+const Homepage = () => {
+  // usestates for inputs
+  const [firstName, setFirstName] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [completeGB, setCompleteGB] = useState([]);
+
+  //   fetch complete guestbook
+  useEffect(() => {
+    fetch("http://localhost:3004/api/v1/guestbook", { method: "GET" })
+      .then((res) => res.json())
+      .then((data) => setCompleteGB(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const addEntry = (event) => {
+    event.preventDefault();
+    const newEntry = {
+      firstName: firstName,
+      name: name,
+      email: email,
+      message: message,
+    };
+    fetch("http://localhost:3004/api/v1/guestbook", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newEntry),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setCompleteGB(data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="home">
+      <h1>GUESTBOOK</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="First-name"
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Last-name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Message"
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button onClick={addEntry}>SUBMIT</button>
+      </form>
+    </div>
+  );
+};
+
+export default Homepage;
